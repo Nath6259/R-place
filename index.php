f0569fbb28b0ac988d1d9a84f4b514de82848f06
Nath6259 <nathan6259115@gmail.com>
2022-09-12 16:25:43 +0200

    R-place

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>R-place</title>
+</head>
+<body>
+    <ol>
+        <li>
+            <h3>R-place</h3>
+            <ol>
+                <li><a href="./Fresque/index.php">Fresques</a></li>
+            </ol>
+        </li>
+</body>
+</html>
